4a613c6cedeb721b0822116c71e1dfc97a086290
Shivansh yadav <shivansh0708@gmail.com>
2020-09-22 15:20:07 +0530

    second commit

--- bolne ke kaam.py
+++ bolne ke kaam.py
@@ -4,16 +4,10 @@ import pyttsx3
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[0].id)
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
-
-f = open("gaana.txt", "rt")
-file = f
-b = f.read()
-speak(b)
-f.close()
-
+a = str(input("Enter the words or things that you want the system to speak:\n"))
+speak(a)
