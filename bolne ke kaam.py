0ec94a6c5c0aaa95cc60ec243ffbd3178ab8e0f6
Shivansh yadav <shivansh0708@gmail.com>
2020-09-22 15:22:42 +0530

    real commit trial

--- bolne ke kaam.py
+++ bolne ke kaam.py
@@ -3,7 +3,7 @@ import pyttsx3
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
+print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
 def speak(audio):
